4fc2c110356dc76c497f25c9d27179bd72f03283
vodatdammecode <166628450+vodatdammecode@users.noreply.github.com>
2024-04-11 21:51:47 +0700

    Update myscript.js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -49,7 +49,7 @@ theTime.add({
 	update: function(percent) {
 		var thePercent = Math.round(percent.progress);
 		// Nội dung của nút bên dưới
-		document.querySelector('.container .sup').innerHTML= "Tim anh đang đập nhanh quá nè " ;
+		document.querySelector('.container .sup').innerHTML= "Tim anh đang đập nhanh quá nè 🤭" ;
 	}
 });
 theTime.add({
